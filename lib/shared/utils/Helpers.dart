96266f864d5d56d4eb9ec4bb19cb9ec57553ddd2
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-11-16 00:10:19 +0530

    Add helper class to manage SharedPreferances

--- lib/shared/utils/Helpers.dart
+++ lib/shared/utils/Helpers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 List<String> extractListFromJson(List<dynamic> list) {
   List<String> finalList = new List();
@@ -17,4 +18,26 @@ class HexColor extends Color {
   }
 
   HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+}
+
+class SharedPrefsHelper {
+  static final  String USER_ID = "USER_ID";
+  static final  String EMAIL = "EMAIL";
+  static final  String MOBILE_NUMBER = "MOBILE_NUMBER";
+  static final  String NAME = "NAME";
+
+  static Future<Null> saveUserData({String userId, String name, String email, String mobileNumber}) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if(userId != null) {
+      await prefs.setString(USER_ID, userId);
+    }
+    if(email != null) {
+      await prefs.setString(EMAIL, email);
+    }
+    if(mobileNumber != null) {
+      await prefs.setString(MOBILE_NUMBER, mobileNumber);
+    }
+    if(name != null) {}
+    await prefs.setString(NAME, name);
+  }
 }

add15fc5e328ce2f4669fea7ffdaa11e267614f2
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-11-18 14:41:18 +0530

    Update Theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 import 'package:go_luggage_free/auth/login/view/LoginScreen.dart';
 import 'package:go_luggage_free/shared/network/GraphQlProvider.dart';
-import 'package:go_luggage_free/shared/utils/Constants.dart' as prefix0;
+import 'package:go_luggage_free/shared/utils/Constants.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 void main() => runApp(MyApp());
@@ -10,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ValueNotifier<GraphQLClient> _graphQlClient = getClient();
-
+    // FlutterStatusbarcolor.setStatusBarColor(Colors.white);
+    FlutterStatusbarcolor.setStatusBarWhiteForeground(false);
     return GraphQLProvider(
       client: _graphQlClient,
       child: CacheProvider(
@@ -25,11 +27,13 @@ class MyApp extends StatelessWidget {
               elevation: 0.0,
             ),
             fontFamily: 'Roboto',
-            buttonColor: prefix0.buttonColor,
+            buttonColor: buttonColor,
             textTheme: TextTheme(
-              title: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold),
+              title: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold, fontFamily: 'Roberto'),
               button: TextStyle(color: Colors.white),
-              overline: TextStyle(color: prefix0.buttonColor, fontFamily: 'Roberto', fontSize: 10.0)
+              overline: TextStyle(color: buttonColor, fontFamily: 'Roberto', fontSize: 10.0),
+              headline: TextStyle(color: Colors.black, fontSize: 16, fontFamily: 'Roberto', fontWeight: FontWeight.bold), 
+              body1: TextStyle(color: Colors.grey, fontFamily: 'Roberto', fontSize: 12)
             )
           ),
           home: LoginPage()

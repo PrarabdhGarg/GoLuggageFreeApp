d53828cb4c5a584fdc86495c13e27268f6c18ad2
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-11-27 02:06:04 +0530

    Modularize code for Coupon data classes

--- lib/CouponSelection/model/Coupon.dart
+++ lib/CouponSelection/model/Coupon.dart
@@ -1,6 +1,6 @@
-class Coupon {
-  String type;
-  int value;
+import 'package:go_luggage_free/CouponSelection/model/DiscountCoupon.dart';
+
+class Coupon implements Comparable{
   String description;
   String title;
   String id;
@@ -8,8 +8,6 @@ class Coupon {
   bool isUseable;
 
   Coupon({
-    this.type,
-    this.value,
     this.title,
     this.description,
     this.expiryTime,
@@ -17,13 +15,56 @@ class Coupon {
     this.isUseable
   });
 
-  factory Coupon.fromJsonResponse(Map<String, dynamic> response, bool isUseable) => Coupon(
-    title: response["title"],
-    type: response["type"],
-    id: response["_id"],
-    description: response["description"],
-    expiryTime: response["expiryTime"],
-    isUseable: isUseable,
-    value: response["value"]
-  );
+  factory Coupon.fromJSON(Map<String, dynamic> response, bool isUseable) {
+    print("Recived Response = ${response.toString()}");
+    String type = response["type"];
+    print("Type = $type");
+    print("${double.parse(response["value"].toString())}\n${response["title"]}\n${response["_id"]}\n${response["description"]}\n${response["expiryTime"]}\n$isUseable");
+    switch(type) {
+      case "DISCOUNT" : {
+        return DiscountCoupon(
+          type: type,
+          value: double.parse(response["value"].toString()),
+          coupon: Coupon(
+            title: response["title"],
+            id: response["_id"],
+            description: response["description"],
+            expiryTime: response["expiryTime"],
+            isUseable: isUseable,
+          ),
+        );
+      }
+      default: {
+        return Coupon(
+          title: response["title"],
+          id: response["_id"],
+          description: response["description"],
+          expiryTime: response["expiryTime"],
+          isUseable: isUseable,
+        );
+      }
+    }
+  }
+
+  int getDiscountedPrice(double orignalPrice) {
+    return orignalPrice.round();
+  }
+
+  @override
+  int compareTo(other) {
+    if(other is Coupon) {
+      if(other != null) {
+        if(this.isUseable && !(other.isUseable))
+          return 1;
+        else if(!(this.isUseable) && other.isUseable)
+          return -1;
+        return 0;
+      }
+    }
+    return null;
+  }
+
+  String toString() {
+    return "Description: ${description}\nid: ${id}";
+  }
 }

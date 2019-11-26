bd5b4441a39dc647e79ad622d4580d9e666fa9c1
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-11-27 02:03:58 +0530

    Add function to calculate number of days

--- lib/shared/utils/Helpers.dart
+++ lib/shared/utils/Helpers.dart
@@ -32,83 +32,10 @@ String getUserReadableTime(DateTime dateTime) {
   return dateTime.hour.toString() + ":" + dateTime.minute.toString();
 }
 
-class SharedPrefsHelper {
-  static final  String USER_ID = "USER_ID";
-  static final  String EMAIL = "EMAIL";
-  static final  String MOBILE_NUMBER = "MOBILE_NUMBER";
-  static final  String NAME = "NAME";
-  static final String JWT = "JWT_TOKEN";
-
-  static Future<Null> saveUserData({String userId, String name, String email, String mobileNumber, @required String jwt}) async {
-    SharedPreferences _prefs = await SharedPreferences.getInstance();
-    if(userId != null) {
-      await _prefs.setString(USER_ID, userId);
-    }
-    if(email != null) {
-      await _prefs.setString(EMAIL, email);
-    }
-    if(mobileNumber != null) {
-      await _prefs.setString(MOBILE_NUMBER, mobileNumber);
-    }
-    if(name != null) {
-      await _prefs.setString(NAME, name);
-    }
-    await _prefs.setString(JWT, jwt);
-  }
-
-  static Future<String> getUserNumber() async {
-    SharedPreferences _prefs = await SharedPreferences.getInstance();
-    var number = await _prefs.getString(MOBILE_NUMBER);
-    if(number != null) {
-      return number;
-    } else {
-      return "";
-    }
-  }
-
-  static Future<String> getUserEmail() async {
-    SharedPreferences _prefs = await SharedPreferences.getInstance();
-    var email = _prefs.getString(EMAIL);
-    if(email != null) {
-      return email;
-    } else {
-      return "";
-    }
-  }
-
-  static Future<bool> checkUserLoginStatus() async {
-    SharedPreferences _prefs = await SharedPreferences.getInstance();
-    var id = _prefs.getString(JWT);
-    if(id != null && id.isNotEmpty) {
-      return true;
-    }
-    return false;
-  }
-
-  static Future<String> getUserId() async {
-    SharedPreferences _prefs = await SharedPreferences.getInstance();
-    var id = _prefs.getString(USER_ID);
-    if(id != null && id.isNotEmpty) {
-      return id;
-    }
-    return "";
-  }
-
-  static Future<String> getJWT() async {
-    SharedPreferences _prefs = await SharedPreferences.getInstance();
-    var id = _prefs.getString(JWT);
-    if(id != null && id.isNotEmpty) {
-      return id;
-    }
-    throw Exception("User not logged in");
-  }
-
-  static Future<String> getUserName() async {
-    SharedPreferences _prefs = await SharedPreferences.getInstance();
-    var name = _prefs.getString(NAME);
-    if(name != null && name.isNotEmpty) {
-      return name;
-    }
-    return "";
-  }
+int calculateNumberOfDays(DateTime start, DateTime end) {
+  Duration diff = end.difference(start).abs();
+  if(diff.inHours % 24 == 0)
+    return diff.inDays;
+  else
+    return (diff.inDays + 1);
 }

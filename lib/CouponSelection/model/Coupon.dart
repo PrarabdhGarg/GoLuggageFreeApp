1c3e8d592b9575100416766c83d2287b527d54b5
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-11-28 19:23:41 +0530

    Change Coupon Inheretance Structure

--- lib/CouponSelection/model/Coupon.dart
+++ lib/CouponSelection/model/Coupon.dart
@@ -1,70 +1,28 @@
-import 'package:go_luggage_free/CouponSelection/model/DiscountCoupon.dart';
+import 'package:go_luggage_free/CouponSelection/model/Coupon.dart';
 
-class Coupon implements Comparable{
-  String description;
-  String title;
-  String id;
-  String expiryTime;
-  bool isUseable;
+class DiscountCoupon extends Coupon {
+  String type;
+  double value;
 
-  Coupon({
-    this.title,
-    this.description,
-    this.expiryTime,
-    this.id,
-    this.isUseable
+  DiscountCoupon({
+    this.type,
+    this.value,
+    title,
+    description,
+    expiryTime,
+    id,
+    isUseable
   });
 
-  factory Coupon.fromJSON(Map<String, dynamic> response, bool isUseable) {
-    print("Recived Response = ${response.toString()}");
-    String type = response["type"];
-    print("Type = $type");
-    print("${double.parse(response["value"].toString())}\n${response["title"]}\n${response["_id"]}\n${response["description"]}\n${response["expiryTime"]}\n$isUseable");
-    switch(type) {
-      case "DISCOUNT" : {
-        return DiscountCoupon(
-          type: type,
-          value: double.parse(response["value"].toString()),
-          coupon: Coupon(
-            title: response["title"],
-            id: response["_id"],
-            description: response["description"],
-            expiryTime: response["expiryTime"],
-            isUseable: isUseable,
-          ),
-        );
-      }
-      default: {
-        return Coupon(
-          title: response["title"],
-          id: response["_id"],
-          description: response["description"],
-          expiryTime: response["expiryTime"],
-          isUseable: isUseable,
-        );
-      }
-    }
-  }
-
   int getDiscountedPrice(double orignalPrice) {
-    return orignalPrice.round();
-  }
-
-  @override
-  int compareTo(other) {
-    if(other is Coupon) {
-      if(other != null) {
-        if(this.isUseable && !(other.isUseable))
-          return 1;
-        else if(!(this.isUseable) && other.isUseable)
-          return -1;
-        return 0;
+    if(isUseable) {
+      double discount = value * 0.01 * orignalPrice;
+      if(discount >= orignalPrice) {
+        throw new Exception("Invalid discount Value");
       }
+      double discountedPrice = orignalPrice = discount;
+      return discountedPrice.round();
     }
-    return null;
-  }
-
-  String toString() {
-    return "Description: ${description}\nid: ${id}";
+    return orignalPrice.round();
   }
 }

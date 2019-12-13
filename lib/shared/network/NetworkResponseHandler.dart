52c49515bacce522dfda61f3c7583e42a9bebbea
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-12-13 10:56:31 +0530

    Add class to handle network responses
    Migration to this class would be done after releasing the app

---
+++ lib/shared/network/NetworkResponseHandler.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:go_luggage_free/shared/network/errors/NetworkErrorChecker.dart';
+import 'package:go_luggage_free/shared/network/errors/NetworkErrorListener.dart';
+import 'package:http/http.dart' as http;
+
+class NetworkRespoonseHandler {
+  static void handleResponse({@required Function(String) onSucess, http.Response response, @required NetworkErrorListener errorListener}) async {
+    if(response != null) {
+      print("Response Status Code = ${response.statusCode}");
+      print("Response body = ${response.body}");
+      if(response.statusCode == 200 || response.statusCode == 201) {
+        onSucess(response.body);
+      } else {
+        if(errorListener != null) {
+          NetworkErrorChecker(respoonseBody: response.body, networkErrorListener: errorListener);
+        }
+        else {
+          throw Exception("Obtained null error listener\n${response.statusCode}\n${response.body}\n${response.request}");
+        }
+      }
+    }
+  }
+}
